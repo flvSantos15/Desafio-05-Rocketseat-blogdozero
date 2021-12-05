6ddf307d26317809e1dee18144ae3b0d8e602721
flvSantos15 <48564704+flvSantos15@users.noreply.github.com>
2021-12-05 11:40:15 -0300

    nao esta funcionando os posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import { GetStaticProps } from 'next';
-import Head from 'next/head'
+import Link from 'next/link';
+import { Header } from '../components/Header'
 import Prismic from '@prismicio/client'
+import { FiCalendar, FiUser } from 'react-icons/fi'
+import { format } from 'date-fns';
+import React, { useState } from 'react';
+import {ptBR} from 'date-fns/locale'
 //importação do stripe
 
 import { getPrismicClient } from '../services/prismic';
@@ -15,7 +20,7 @@ interface Post {
     title: string;
     subtitle: string;
     author: string;
-  };
+  }
 }
 
 interface PostPagination {
@@ -27,32 +32,86 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home({postsPagination}: HomeProps) {
-  return(
+export default function Home({ postsPagination }: HomeProps) : JSX.Element {
+  const formattedPost = postsPagination.results.map(post => {
+    return {
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    }
+  })
+  
+  const [posts, setPosts] = useState<Post[]>(formattedPost)
+  return (
     <>
-      <Head>
-        {/* Logo */}
-        <img src='./images/logo.svg'/>
-      </Head>
-      <main className={styles.contentContainer}>
-        {/* posts */}
-        <section>
-          <h4>text</h4>
-        </section>
+      <main className={commonStyles.container}>
+        <Header />
+
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
+              <a className={styles.post}>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <ul>
+                  <li>
+                    <FiCalendar />
+                    {post.first_publication_date}
+                  </li>
+                  
+                  <li>
+                    <FiUser />
+                    {post.data.author}
+                  </li>
+                </ul>
+              </a>
+            </Link>
+          ))}
+
+          <button type='button'>
+            Carregar mais posts
+          </button>
+        </div>
+
       </main>
 
     </>
   )
 }
 
-export const getStaticProps = async () => {
-  const prismic = getPrismicClient();
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
   const postsResponse = await prismic.query([
-    Prismic.predicates.at('document.type', 'pos') // pos é o id que vem do prismic
+    Prismic.Predicates.at('document.type', 'posts'),
   ], {
-    fetch: ['title', 'content'],
-    pageSize: 100,
-  });
+    pageSize: 1,
+  })
 
-  // TODO
-};
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    }
+  })
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  }
+
+  return {
+    props: {
+      postsPagination,
+    }
+  }
+}
