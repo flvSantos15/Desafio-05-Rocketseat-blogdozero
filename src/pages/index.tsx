58564493c53173118214cd45af6d7ab0a7712fa3
flvSantos15 <48564704+flvSantos15@users.noreply.github.com>
2021-12-05 14:12:44 -0300

    erro resolvido

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,6 +47,40 @@ export default function Home({ postsPagination }: HomeProps) : JSX.Element {
   })
   
   const [posts, setPosts] = useState<Post[]>(formattedPost)
+
+  const [nextPage, setNextPage] = useState(postsPagination.next_page)
+
+  const [currentPage, setCurrentPage] = useState(1)
+
+  async function handleNextPage(): Promise<void> {
+    if(currentPage !== 1 && nextPage === null){
+      return
+    }
+    const postsResults = await fetch(`${nextPage}`).then(response => response.json())
+
+    setNextPage(postsResults.next_page)
+    setCurrentPage(postsResults.page)
+
+    const newPosts = postsResults.results.map(post => {
+      return{
+        uid: post.uid,
+        first_publication_date: format(
+          new Date(post.first_publication_date),
+          'dd MMM yyyy',
+          {
+            locale: ptBR,
+          }
+        ),
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        }
+      }
+    })
+  }
+
+  //parei no 50:50
   return (
     <>
       <main className={commonStyles.container}>
@@ -73,7 +107,10 @@ export default function Home({ postsPagination }: HomeProps) : JSX.Element {
             </Link>
           ))}
 
-          <button type='button'>
+          <button 
+            type='button'
+            onClick={handleNextPage}
+          >
             Carregar mais posts
           </button>
         </div>
